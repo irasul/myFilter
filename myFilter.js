c1b9b445ca082f9a47562aa896ee8a3c4d693af1
irasul <irasul@crosscap.com>
2017-04-26 14:46:16 -0400

    performance improvement

--- myFilter.js
+++ myFilter.js
@@ -1,24 +1,56 @@
+"use strict";
 app.directive('myFilter',function(){
 	
 	return {
 		restrict: 'A',
-		templateUrl: "filter_template.html",
-		scope:true,
+		template: `
+			<a ng-click="toggleFilters($id)">{{title}}</a>
+			<div class="options" id="opt-{{$id}}">
+				<input ng-model="srchFilter" class="srchFilter" placeholder="Search {{title}}" ng-change="searchList(this)">	
+				<div class="options_holder">
+					<div class="checkbox ng-hide" ng-repeat="r in data" ng-show="r.show">
+						<label>
+							<input type="checkbox" name="{{title}}Filter" type="checkbox" ng-value="r.name"  checked="checked" control="{{control}}" onchange="filter_x_0_1(this)"/><span ng-bind="r.name"></span> 
+						</label>
+					</div>
+				</div>	
+				<div class='clearfix footer'>
+					<div style='float:left'>Select <a ng-click="selectAll()">All</a> | <a ng-click="selectNone()">None</a></div> 
+					<div style='float:right'><a class="btn" ng-click="toggleFilters($id)">Done</a></div>
+				</div>	
+			</div>
+		`,
+		scope:{
+			title: "@",
+			control: "@",
+			keys: "@",
+			dataset: "="
+		},
 		compile: function(element, attributes){
  
 			return {
 	 			function(scope, element, attributes){
-					
+						
 	            }
 	         }
-     },
-     controller: function($scope,$element,$attrs){
-		$scope.title = $attrs.title;
-		$scope.control = $attrs.control;
+    },
+    controller: function($scope,$element,$attrs){
+		$element.addClass('filter');
+		$scope.datacol = $scope.dataset.map(i => {return i[$scope.keys]});
+		$scope.data=[];
+		for(let i = 0 ; i < $scope.datacol.length ; i++){
+			let subAry = $scope.data.filter( item => { return item.name == $scope.datacol[i]})	
+			if(!subAry.length){
+				$scope.data.push({ name: $scope.datacol[i], show: true});
+			}
+		}
+
+		/*$scope.ary=$scope.$parent[$scope.data];*/
+
 		//console.log($scope.control);
-		$scope.$parent.$watch($attrs.data,function(newValue, oldValue){
+		/*$scope.$parent.$watch($scope.data,function(newValue, oldValue){
 				if(newValue != oldValue){
-					$scope.ary=$scope.$parent[$attrs.data];
+					$scope.ary=$scope.$parent[$scope.data];
 					$scope.aryShow = [];
 					($scope.ary).forEach(function(){
 						$scope.aryShow.push(true);
@@ -26,16 +58,12 @@ app.directive('myFilter',function(){
 					
 				}
 				
-		},true);
-		$scope.showFilters = function($event){
-			//angular.element($event.currentTarget).find(".options").toggle();
-			var elm = $event.target.nextElementSibling.style;
+		},true);*/
+		$scope.toggleFilters = function(id){
+			var elm = document.getElementById('opt-'+id).style;
 			elm.display=elm.display=="block"?"none":"block";
 		}
-		$scope.hideFilters = function($event){
-			var elm = $event.target.parentElement.style;
-			elm.display="none";
-		}
+		
 		$scope.selectAll = function(){
 
 			$("input[name='"+$scope.title+"Filter']").each(function(){
@@ -52,19 +80,19 @@ app.directive('myFilter',function(){
 
 		$scope.searchList = function(){
 			if($scope.srchFilter==""){
-				$.each($scope.ary,function(e){
-					$scope.aryShow[e] = true;
+				$scope.data.forEach(function(e){
+					$scope.data[e].show = true;
 				});
 			}
 			else{			
 				var r = new RegExp($scope.srchFilter,"gi");
-				$.each($scope.ary,function(e){
-					var s = new String($scope.ary[e]);
+				$scope.data.forEach(function(e){
+					var s = new String($scope.data[e].name);
 					if(s.match(r)!=null){
-						$scope.aryShow[e] = true;
+						$scope.data[e].show=true;
 					}
 					else{
-						$scope.aryShow[e] = false;
+						$scope.data[e].show=false;
 					}	
 				});
 			}	
